fc45094ac52b08b07241348d7395cc0819cf7ed1
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-05-24 23:32:24 +0600

    get method for  menu and review done

--- bistro-boss-server/index.js
+++ bistro-boss-server/index.js
@@ -25,7 +25,18 @@ async function run() {
     try {
         await client.connect();
 
+        const reviewCollection = client.db("bistroDB").collection("reviews");
+        const menuCollection = client.db("bistroDB").collection("menu");
 
+        app.get('/reviews', async (req, res) => {
+            const result = await reviewCollection.find().toArray()
+            res.send(result)
+        })
+
+        app.get('/menu', async (req, res) => {
+            const result = await menuCollection.find().toArray()
+            res.send(result)
+        })
 
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
