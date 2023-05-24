91a0292d8c2b3f3679026f7dca83a6b59decb184
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-05-24 22:13:33 +0600

    userName and userPass added

--- bistro-boss-server/index.js
+++ bistro-boss-server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 7000
 
@@ -10,9 +11,8 @@ app.use(express.json())
 
 
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.mf37tl1.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_TITLE}:${process.env.DB_PASS}@cluster0.mf37tl1.mongodb.net/?retryWrites=true&w=majority`
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -23,9 +23,10 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-        // Send a ping to confirm a successful connection
+
+
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
