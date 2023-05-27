b96d558d31c0a1ba245c3bde5eb008e507177828
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-05-28 01:26:43 +0600

    get doctors data done

---
+++ doctor-server/index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+const app = express();
+const port = process.env.PORT || 8000;
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_TITLE}:${process.env.DB_PASS}@cluster0.mf37tl1.mongodb.net/?retryWrites=true&w=majority`
+
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+
+        const doctorsCollections = client.db("doctorDB").collection("doctors");;
+
+        app.get('/doctors', async (req, res) => {
+            const doctors = await doctorsCollections.find().toArray()
+            res.send(doctors)
+        })
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('doctors')
+})
+app.listen(port, () => {
+    console.log(`PORT is running in - ${port}`)
+})
