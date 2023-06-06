34e5c46d46efcf929781752b9f8198f76105e5f2
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-06-06 09:54:12 +0600

    mongodb driver added

--- aircnc/index.js
+++ aircnc/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 1000
 
@@ -10,10 +11,39 @@ app.use(cors())
 app.use(express.json())
 
 
-app.get('/',(req,res) => {
+
+const uri = "mongodb+srv://<username>:<password>@cluster0.mf37tl1.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+async function run() {
+    try {
+        await client.connect();
+
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
     res.send('Aircbc')
 })
 
-app.listen(port , () => {
+app.listen(port, () => {
     console.log(`port is running in - ${port}`)
 })
