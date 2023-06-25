a5079625f4a4e6ac6de79f3c45bf09cf2da931cd
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-06-26 01:36:57 +0600

    get features and new products done

--- Glamour-Attire/index.js
+++ Glamour-Attire/index.js
@@ -1,16 +1,55 @@
 const express = require('express');
-const cors = require('cors');
 const app = express()
+const cors = require('cors');
+require('dotenv').config()
 const port = process.env.PORT || 3000
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // Middle Ware
 app.use(express.json())
 app.use(cors())
 
-app.get('/',(req,res)=>{
+
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_KEY}@cluster0.mf37tl1.mongodb.net/?retryWrites=true&w=majority`
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // await client.connect();
+
+        const allProductsColletion = client.db("glamour-attire").collection("products")
+
+        app.get('/products/features',async(req,res) => {
+            const result = await allProductsColletion.find({type:'f'}).toArray()
+            res.send(result)
+        })
+
+        app.get('/products/new',async(req,res) => {
+            const result = await allProductsColletion.find({type:'n'}).toArray()
+            res.send(result)
+        })
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
     res.send('Glamour Attire')
 })
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`port is running in ${port}`)
 })
